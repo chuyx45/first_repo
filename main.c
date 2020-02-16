55a3ff4f6a5317a3ef5b88d8b4cca3dd10e3334d
chuyx45 <60621832+chuyx45@users.noreply.github.com>
2020-02-16 10:16:19 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 //comentario
 int main(){
-  if(1)
-    printf("Hola\n");
-  else 
-    printf("Adios\n");
+  for(int i=0; i<6; i++)
+    printf("Hola :%i",i);
+  
+  //fin
   return 0;
 }
